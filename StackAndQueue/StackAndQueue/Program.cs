8ac7817c53337cb32836bbdae920eb19849eda6b
Amol <ajamnik.mech@gmail.com>
2023-01-17 22:23:13 +0530

    [Amol]- Create Queue in Enqueue

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -11,20 +11,28 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
 
-            Stack st = new Stack();
-            st.Push(70);
-            st.Push(30);
-            st.Push(56);
-            st.display();
-            st.Peek();
-            st.Pop();
-            st.display();
-            st.Peek();
-            st.Pop();
-            st.display();
-            st.Peek();
-            st.Pop();
-            st.display();
+            //Stack st = new Stack();
+            //st.Push(70);
+            //st.Push(30);
+            //st.Push(56);
+            //st.display();
+            //st.Peek();
+            //st.Pop();
+            //st.display();
+            //st.Peek();
+            //st.Pop();
+            //st.display();
+            //st.Peek();
+            //st.Pop();
+            //st.display();
+
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
+            
         }
     }
 }
